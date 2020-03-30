bb812eb90c7ad9c3c8d0f0b01e3bca835abcb501
Dennis Gosnell <cdep.illabout@gmail.com>
2020-03-30 11:43:13 +0900

    update

---
+++ shell.nix
@@ -0,0 +1,18 @@
+
+let
+  nixpkgs-src = builtins.fetchTarball {
+    # Recent version of nixpkgs master as of 2020-03-30.
+    url = "https://github.com/NixOS/nixpkgs/archive/570e3edc8519c666b74a5ca469e1dd286902691d.tar.gz";
+    sha256 = "sha256:0aw6rw4r13jij8hn27z2pbilvwzcpvaicc59agqznmr2bd2742xl";
+  };
+
+  nixpkgs = import nixpkgs-src {};
+in
+
+with nixpkgs;
+
+let
+  pythonEnv = python37.withPackages (ps: [ps.PyGithub]);
+in
+pythonEnv.env
+
