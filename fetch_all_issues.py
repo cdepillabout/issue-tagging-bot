84b8e0fddf5f1c892bee4d67a95d714f49240040
Dennis Gosnell <cdep.illabout@gmail.com>
2020-04-03 18:22:15 +0900

    update

--- fetch_all_issues.py
+++ fetch_all_issues.py
@@ -5,7 +5,9 @@ import sys
 from datetime import datetime, timedelta, timezone
 from time import sleep
 
-from github import Github
+# PyGithub has gotten mypy types, but it has not been released yet:
+# https://github.com/PyGithub/PyGithub/pull/1231
+from github import Github, Repository # type: ignore
 
 
 class MyEncoder(json.JSONEncoder):
@@ -83,37 +85,40 @@ class IssueData:
 
 
 class RateLimiter:
-    def __init__(self, github):
+    def __init__(self, github: Github):
         self.github = github
 
-    def maybe_wait(self, buffer_amount=10):
+    def maybe_wait(self, buffer_amount: int = 10) -> None:
         # get the current limit
         rate_limit = self.github.get_rate_limit()
         remaining = rate_limit.core.remaining
 
         if remaining < buffer_amount:
 
+            # The time at which the rate limit is reset.
             reset_time = rate_limit.core.reset
-            utc_reset_time = reset_time.replace(tzinfo=timezone.utc)
 
+            # When to sleep until.  This is just reset_time plus one minute.
             sleep_until_time = reset_time + timedelta(minutes=1)
+
+            # When to sleep until with a UTC timezone set.
             utc_sleep_until_time = sleep_until_time.replace(tzinfo=timezone.utc)
 
             now = datetime.utcnow()
-            utc_now = now.replace(tzinfo=timezone.utc)
 
+            # How much time until the the rate limit is reset.
             sleep_delta = sleep_until_time - now
 
             print(f"github rate limit remaining requests is {remaining},")
             print(f"which is less than the buffer amount: {buffer_amount},")
             print(
-                f"so sleeping for {sleep_delta.seconds} seconds, until {sleep_until_time.astimezone()}..."
+                f"so sleeping for {sleep_delta.seconds} seconds, until {utc_sleep_until_time.astimezone()}..."
             )
 
             sleep(sleep_delta.seconds)
 
 
-def main():
+def main() -> None:
 
     github = Github()
 
@@ -123,16 +128,16 @@ def main():
 
     sys.exit()
 
-    repo = github.get_repo("NixOS/nixpkgs")
+    repo: Repository = github.get_repo("NixOS/nixpkgs")
     l = repo.get_issues(state="all")
     print(l[0])
     first_issue = l[0]
 
     print(MyEncoder().encode(IssueData.from_issue(l[6])))
 
-    # for i in l:
-    #     print()
-    #     print(MyEncoder().encode(IssueData.from_issue(i)))
+    for i in l:
+        print()
+        print(MyEncoder().encode(IssueData.from_issue(i)))
 
 
 if __name__ == "__main__":
