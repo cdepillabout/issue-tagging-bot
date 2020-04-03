e09d12bcb61cf549b2be039047b5579c63e92c63
Dennis Gosnell <cdep.illabout@gmail.com>
2020-04-03 15:07:44 +0900

    update

--- fetch_all_issues.py
+++ fetch_all_issues.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
 import json
+import sys
 
+from datetime import timezone
 from github import Github
 
-g = Github()
-repo = g.get_repo("NixOS/nixpkgs")
+github = Github()
+
+rate_limit = github.get_rate_limit()
+print(rate_limit)
+print(rate_limit.core)
+print(rate_limit.core.reset)
+print(rate_limit.core.reset.tzinfo)
+
+reset_time = rate_limit.core.reset
+utc_reset_time = reset_time.replace(tzinfo=timezone.utc)
+
+print(utc_reset_time.astimezone())
+
+sys.exit()
+
+repo = github.get_repo("NixOS/nixpkgs")
 l = repo.get_issues(state="all")
 print(l[0])
 first_issue = l[0]
@@ -13,7 +29,6 @@ first_issue = l[0]
 class MyEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, LabelData):
-            print("LLLLLLLLLLLLLLLLLLLLLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
             return o.__dict__
         if isinstance(o, IssueData):
             return o.__dict__
@@ -24,9 +39,10 @@ class LabelData():
         self.name = name
         self.url = url
 
-def label_to_label_data(label):
-    label_data = LabelData(label.name, label.url)
-    return label_data
+    @classmethod
+    def from_label(cls, label):
+        label_data = cls(label.name, label.url)
+        return label_data
 
 class IssueData():
     def __init__(self, id_, url, number, state, title, body,
@@ -40,34 +56,24 @@ class IssueData():
         self.body = body
         self.user_login = user_login
         self.user_id = user_id
-        self.labels = list(map(label_to_label_data, labels))
+        self.labels = list(map(LabelData.from_label, labels))
         self.comments = comments
         self.closed_at = None if closed_at is None else str(closed_at)
         self.created_at = None if created_at is None else str(created_at)
         self.updated_at = None if updated_at is None else str(updated_at)
         self.is_pull_request  = pull_request is None
 
-def issue_to_issue_data(issue):
-    issue_data = IssueData(issue.id, issue.url, issue.number, issue.state,
-            issue.title, issue.body, issue.user.login, issue.user.id,
-            issue.labels, issue.comments, issue.closed_at, issue.created_at,
-            issue.updated_at, issue.pull_request)
-    return issue_data
+    @classmethod
+    def from_issue(cls, issue):
+        issue_data = cls(issue.id, issue.url, issue.number, issue.state,
+                issue.title, issue.body, issue.user.login, issue.user.id,
+                issue.labels, issue.comments, issue.closed_at, issue.created_at,
+                issue.updated_at, issue.pull_request)
+        return issue_data
 
-# print(first_issue.pull_request)
-# print(l[1].pull_request)
-# print(l[2].pull_request)
-# print(l[3].pull_request)
-# print(l[4].pull_request)
-# print(l[5].pull_request)
-# print(l[6].pull_request)
+print(MyEncoder().encode(IssueData.from_issue(l[6])))
 
-issue_data = issue_to_issue_data(first_issue)
-# print(json.dumps(issue_data.__dict__))
-# print(MyEncoder().encode(issue_data))
-# print(json.dumps(.__dict__))
-print(MyEncoder().encode(issue_to_issue_data(l[6])))
+# for i in l:
+#     print()
+#     print(MyEncoder().encode(IssueData.from_issue(i)))
 
-for i in l:
-    print()
-    print(MyEncoder().encode(issue_to_issue_data(i)))
