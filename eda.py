bed245638a41f78098030705eb0c6901fa2faace
Dennis Gosnell <cdep.illabout@gmail.com>
2020-04-05 22:18:58 +0900

    update

--- eda.py
+++ eda.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pandas as pd  # type: ignore
 from sklearn.preprocessing import MultiLabelBinarizer  # type: ignore
 
 from issue_tagging_bot.issue_data import issue_data_frame
 
+def only_topics():
+    only_issues_with_labels = issues_with_topics()
+    return only_issues_with_labels.iloc[:, 14:]
 
-def main() -> None:
+def issues_with_topics():
+    only_issues, topic_classes, topics = main()
+    only_issues_with_labels = pd.concat([only_issues, topics], axis="columns")
+    return only_issues_with_labels
+
+def main():
     all_issue_data = issue_data_frame()
 
     only_issues = all_issue_data[all_issue_data.is_issue]
-    issues_series: pd.Series = only_issues.labels.apply(lambda val: set(map(lambda l: l["name"], val)))
-    print(issues_series)
-    print()
+    label_series: pd.Series = only_issues.labels.apply(lambda val: set(map(lambda l: l["name"], val)))
 
     mlb = MultiLabelBinarizer()
-    res = mlb.fit_transform(issues_series)
-    print(res)
-    print()
-    print(mlb.classes_)
+    res = mlb.fit_transform(label_series)
 
-    for c in mlb.classes_:
-        print(c)
+    topic_label_selector = np.char.startswith(mlb.classes_.astype(str), "6.")
+    topic_classes =  mlb.classes_[topic_label_selector]
+    topic_labels = res[:, topic_label_selector]
+    topics = pd.DataFrame(data=topic_labels, columns=topic_classes)
 
-    return
+    only_issues_indicies = only_issues.index
+    topics = topics.set_index(only_issues_indicies)
+
+    return only_issues, topic_classes, topics
 
 
 if __name__ == "__main__":
